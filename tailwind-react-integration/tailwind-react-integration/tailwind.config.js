ca37695867429fa300f37a7d458a4ea23e6fa5f3
justuskimani <kimanijustus090@gmail.com>
2024-12-09 00:30:44 +0400

    commit

--- tailwind-react-integration/tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind-react-integration/tailwind.config.js
@@ -1,3 +1,5 @@
+@tailwind base;
+@tailwind components;
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Paths to scan for class names
